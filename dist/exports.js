97f535ba4cc675b09484556dd3f7485b0eb8199b
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-08-28 14:38:45 -0600

    Release 0.2.0

--- dist/exports.js
+++ dist/exports.js
@@ -9,16 +9,102 @@ Object.defineProperty(exports, "CompleteIcon", {
     return _CompleteIcon.default;
   }
 });
+Object.defineProperty(exports, "CourseLicense", {
+  enumerable: true,
+  get: function () {
+    return _courseLicense.default;
+  }
+});
 Object.defineProperty(exports, "Sequence", {
   enumerable: true,
   get: function () {
     return _sequence.default;
   }
 });
+Object.defineProperty(exports, "checkBlockCompletion", {
+  enumerable: true,
+  get: function () {
+    return _data3.checkBlockCompletion;
+  }
+});
+Object.defineProperty(exports, "courseHomeReducer", {
+  enumerable: true,
+  get: function () {
+    return _data.reducer;
+  }
+});
+Object.defineProperty(exports, "coursewareReducer", {
+  enumerable: true,
+  get: function () {
+    return _slice.reducer;
+  }
+});
+Object.defineProperty(exports, "fetchCourse", {
+  enumerable: true,
+  get: function () {
+    return _data3.fetchCourse;
+  }
+});
+Object.defineProperty(exports, "fetchSequence", {
+  enumerable: true,
+  get: function () {
+    return _data3.fetchSequence;
+  }
+});
+Object.defineProperty(exports, "getResumeBlock", {
+  enumerable: true,
+  get: function () {
+    return _data3.getResumeBlock;
+  }
+});
+Object.defineProperty(exports, "getSequenceForUnitDeprecated", {
+  enumerable: true,
+  get: function () {
+    return _data3.getSequenceForUnitDeprecated;
+  }
+});
+Object.defineProperty(exports, "modelsReducer", {
+  enumerable: true,
+  get: function () {
+    return _modelStore.reducer;
+  }
+});
+Object.defineProperty(exports, "recommendationsReducer", {
+  enumerable: true,
+  get: function () {
+    return _slice2.reducer;
+  }
+});
+Object.defineProperty(exports, "saveSequencePosition", {
+  enumerable: true,
+  get: function () {
+    return _data3.saveSequencePosition;
+  }
+});
+Object.defineProperty(exports, "toursReducer", {
+  enumerable: true,
+  get: function () {
+    return _data2.reducer;
+  }
+});
 
 var _sequence = _interopRequireDefault(require("./courseware/course/sequence"));
 
 var _CompleteIcon = _interopRequireDefault(require("./courseware/course/sequence/sequence-navigation/CompleteIcon"));
+
+var _data = require("./course-home/data");
+
+var _slice = require("./courseware/data/slice");
+
+var _slice2 = require("./courseware/course/course-exit/data/slice");
+
+var _data2 = require("./product-tours/data");
+
+var _modelStore = require("./generic/model-store");
+
+var _courseLicense = _interopRequireDefault(require("./courseware/course/course-license"));
+
+var _data3 = require("./courseware/data");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 //# sourceMappingURL=exports.js.map
