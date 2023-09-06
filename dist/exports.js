b5aa07d594064914f166e201ba76f08af972be00
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-09-06 12:53:36 -0600

    Release 0.3.0

--- dist/exports.js
+++ dist/exports.js
@@ -3,28 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "CompleteIcon", {
-  enumerable: true,
-  get: function () {
-    return _CompleteIcon.default;
-  }
-});
-Object.defineProperty(exports, "CourseLicense", {
-  enumerable: true,
-  get: function () {
-    return _courseLicense.default;
-  }
-});
 Object.defineProperty(exports, "Sequence", {
   enumerable: true,
   get: function () {
     return _sequence.default;
   }
 });
+Object.defineProperty(exports, "appendBrowserTimezoneToUrl", {
+  enumerable: true,
+  get: function () {
+    return _utils.appendBrowserTimezoneToUrl;
+  }
+});
 Object.defineProperty(exports, "checkBlockCompletion", {
   enumerable: true,
   get: function () {
-    return _data3.checkBlockCompletion;
+    return _data2.checkBlockCompletion;
   }
 });
 Object.defineProperty(exports, "courseHomeReducer", {
@@ -39,28 +33,34 @@ Object.defineProperty(exports, "coursewareReducer", {
     return _slice.reducer;
   }
 });
+Object.defineProperty(exports, "executeThunk", {
+  enumerable: true,
+  get: function () {
+    return _utils.executeThunk;
+  }
+});
 Object.defineProperty(exports, "fetchCourse", {
   enumerable: true,
   get: function () {
-    return _data3.fetchCourse;
+    return _data2.fetchCourse;
   }
 });
 Object.defineProperty(exports, "fetchSequence", {
   enumerable: true,
   get: function () {
-    return _data3.fetchSequence;
+    return _data2.fetchSequence;
   }
 });
 Object.defineProperty(exports, "getResumeBlock", {
   enumerable: true,
   get: function () {
-    return _data3.getResumeBlock;
+    return _data2.getResumeBlock;
   }
 });
 Object.defineProperty(exports, "getSequenceForUnitDeprecated", {
   enumerable: true,
   get: function () {
-    return _data3.getSequenceForUnitDeprecated;
+    return _data2.getSequenceForUnitDeprecated;
   }
 });
 Object.defineProperty(exports, "modelsReducer", {
@@ -69,42 +69,24 @@ Object.defineProperty(exports, "modelsReducer", {
     return _modelStore.reducer;
   }
 });
-Object.defineProperty(exports, "recommendationsReducer", {
-  enumerable: true,
-  get: function () {
-    return _slice2.reducer;
-  }
-});
 Object.defineProperty(exports, "saveSequencePosition", {
   enumerable: true,
   get: function () {
-    return _data3.saveSequencePosition;
-  }
-});
-Object.defineProperty(exports, "toursReducer", {
-  enumerable: true,
-  get: function () {
-    return _data2.reducer;
+    return _data2.saveSequencePosition;
   }
 });
 
 var _sequence = _interopRequireDefault(require("./courseware/course/sequence"));
 
-var _CompleteIcon = _interopRequireDefault(require("./courseware/course/sequence/sequence-navigation/CompleteIcon"));
-
 var _data = require("./course-home/data");
 
 var _slice = require("./courseware/data/slice");
 
-var _slice2 = require("./courseware/course/course-exit/data/slice");
-
-var _data2 = require("./product-tours/data");
-
 var _modelStore = require("./generic/model-store");
 
-var _courseLicense = _interopRequireDefault(require("./courseware/course/course-license"));
+var _data2 = require("./courseware/data");
 
-var _data3 = require("./courseware/data");
+var _utils = require("./utils");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 //# sourceMappingURL=exports.js.map
